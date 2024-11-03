8f802b9d55e884abcec3c9d2fcba8b0c3d1ba388
DDG000 <dilshanguruge99930@gmail.com>
2024-11-03 21:23:40 +0530

    place order cart is ok

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -9,7 +9,7 @@ public class Starter extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/ProductRegister.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/PlaceOrder.fxml"))));
         stage.show();
     }
 }
